6d5764ab93b43e55b9ecfd0ab1ee166e37a25a73
CintyaAprilaFandini <cintyaacaf07@gmail.com>
2022-02-14 10:38:36 +0700

    change welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,19 +22,12 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <div class ="ml-4 text-lg leading-7 font-semibold">
+                <h1 class="text-grey-900 dark:text-white">
+                    Cintya Aprila Fandini <br><br>
+                    kelas: TI 2H, Nim: 204172049
+        </h1></div>
+        </div>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
